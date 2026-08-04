f12a701e3849b3402d8cf3bafae9a0abfef7ed33
cinat3140-rgb <cinat3140-rgb@users.noreply.github.com>
2026-08-05 00:23:49 +0300

    Statik varlik yollari goreliye cevrilir (GitHub Pages alt klasoru); catalog appUpdate notlari duzeltilir

--- app.js
+++ app.js
@@ -95,7 +95,7 @@
   }
 
   function fetchAppUpdate() {
-    fetch("/app-update.json", { headers: { Accept: "application/json" } })
+    fetch("app-update.json", { headers: { Accept: "application/json" } })
       .then(function (r) { return r.ok ? r.json() : null; })
       .then(function (d) {
         if (d && d.updateAvailable) renderUpdateBanner(d.latest);
